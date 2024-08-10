cfc59cc99ae1623b1d37c72ec24ec33fcfcb80a2
Ayush Jindal <104627503+jindalayush326@users.noreply.github.com>
2024-08-10 09:14:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      width: {
+        '460px': '460px',
+      },
+    },
+  },
+  plugins: [],
+
+  
+}
+
